9687bae3030760e89750d372e4d80a9a3ee0fafb
Pedro Castro <contatoaugustopedro@gmail.com>
2023-05-16 21:28:58 -0300

    Estudos usando NodeJS

---
+++ index.js
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    return resultados;
+}
+
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
+}
+
+//async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(extraiLinks(texto))
+    } catch (erro) {
+        trataErro(erro);
+    } finally {
+        console.log(chalk.yellow('Operação conclúida!'))
+    }
+}
+
+
+//promises com then()
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises.readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => {console.log(chalk.green(texto))})
+//     .catch(trataErro)
+// }
+
+pegaArquivo('./arquivos/texto.md');
+
+//  \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
