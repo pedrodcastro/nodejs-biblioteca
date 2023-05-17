240e133c1b006b382344230d61d225126a422c61
Pedro Castro <contatoaugustopedro@gmail.com>
2023-05-17 15:15:48 -0300

    Estudos com NodeJS na criação de uma biblioteca de validação de links

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function extraiLinks(texto) {
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
     const capturas = [...texto.matchAll(regex)];
     const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
-    return resultados;
+    return resultados.length !== 0 ? resultados : 'Não há links no arquivo!';
 }
 
 function trataErro(erro) {
@@ -19,14 +19,13 @@ async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(extraiLinks(texto))
+        return extraiLinks(texto);
     } catch (erro) {
         trataErro(erro);
-    } finally {
-        console.log(chalk.yellow('Operação conclúida!'))
     }
 }
 
+export default pegaArquivo;
 
 //promises com then()
 
@@ -36,7 +35,3 @@ async function pegaArquivo(caminhoDoArquivo) {
 //     .then((texto) => {console.log(chalk.green(texto))})
 //     .catch(trataErro)
 // }
-
-pegaArquivo('./arquivos/texto.md');
-
-//  \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
